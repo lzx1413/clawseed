15f7beeb4b89c9d922e463ef74ed5765173be476
lzx1413 <lzx1413@live.cn>
2026-05-05 03:05:39 +0800

    chore: bump clawseed to v0.1.1, Android app to v1.1

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.clawseed.demo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         buildConfigField("String", "BUILD_DATE", "\"$buildDate\"")
     }
 
